76b849eb77a9909dca76482a4509feaf277cc912
rezilience <aj.gizmofreak@gmail.com>
2019-05-17 18:35:13 -0400

    More playing around

---
+++ misc/misc.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r rot13Reader) Read(b []byte) (n int, err error) {
+	n, err = r.r.Read(b)
+	return
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+	fmt.Println("Hello Go!")
+}
+
+func printSlice(s []byte) {
+	fmt.Printf("len=%d cap=%d; %v\n", len(s), cap(s), s)
+}
