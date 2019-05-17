b6f6c3fd056ee5eadb3a75cf1537747029e9501c
rezilience <aj.gizmofreak@gmail.com>
2019-05-17 18:45:52 -0400

    Create rot13.go

---
+++ tour/rot13.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r rot13Reader) Read(b []byte) (n int, err error) {
+	n, err = r.r.Read(b)
+	return
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
