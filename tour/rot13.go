50266e4827697344023a6ba2815bb44d5ef3e957
rezilience <aj.gizmofreak@gmail.com>
2019-05-17 19:12:22 -0400

    playing around

--- tour/rot13.go
+++ tour/rot13.go
@@ -12,7 +12,24 @@ type rot13Reader struct {
 
 func (r rot13Reader) Read(b []byte) (n int, err error) {
 	n, err = r.r.Read(b)
+	for i := 0; i < n; i++ {
+		b[i] = rot13(b[i])
+	}
 	return
+}
+
+func rot13(b byte) byte {
+	var a byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a = 'a'
+	case 'A' <= b && b <= 'Z':
+		a = 'A'
+	default:
+		return b
+	}
+
+	return (b-a+13)%26 + a
 }
 
 func main() {
